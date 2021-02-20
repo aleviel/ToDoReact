d365f4e37a5332282415dd5897e377fa06ac1bc3
Alexey R <alex666alex@mail.ru>
2021-02-20 08:25:04 +0300

    init

---
+++ ToDoReactApp/src/components/app-filter/app-filter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AppFiler() {
+	return (
+		<>
+			<button>first</button>
+			<button>second</button>
+		</>
+	)
+};
---
+++ ToDoReactApp/src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import AppFilter from '../app-filter';
+
+export default function App() {
+	return (
+		<div>
+			<AppHeader />
+			<AppFilter />
+		</div>
+	)
+};
