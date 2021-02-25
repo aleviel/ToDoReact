c89636a573a61c7826cecdd98026a7959cda53f1
Alexey R <alex666alex@mail.ru>
2021-02-25 12:02:41 +0300

    add post

--- ToDoReactApp/src/components/app-add-form/app-add-form.js
+++ ToDoReactApp/src/components/app-add-form/app-add-form.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './app-add-form.css'
 
-export default function AppAddForm({ addFunc }) {
-	return (
 
-		<div className='botton-panel d-flex'>
-			<input
-				type="text"
-				className='form-control new-post-label'
-				placeholder='type here...' />
-			<button
-				type='submit'
-				className='btn btn-outline-secondary'
-				onClick={() => { addFunc('fasdfa') }}>
-				Add
-			</button>
-		</div >
 
-	)
-};
+export default class AppAddForm extends Component {
+	state = {
+		inputBody: ''
+	}
+
+	onTextChange = (e) => {
+		this.setState({
+			inputBody: e.target.value
+		})
+	}
+
+	onSubmit = (e) => {
+		e.preventDefault()
+		this.props.addFunc(this.state.inputBody)
+		this.setState({
+			inputBody: ''
+		})
+	}
+
+	render() {
+		const { inputBody } = this.state;
+		return (
+			<form className='botton-panel d-flex'
+				onSubmit={this.onSubmit}>
+				<input
+					type="text"
+					className='form-control new-post-label'
+					placeholder='type here...'
+					onChange={this.onTextChange}
+					value={inputBody} />
+				<button
+					type='submit'
+					className='btn btn-outline-secondary'>
+					Add
+				</button>
+			</form >
+		);
+	}
+}
